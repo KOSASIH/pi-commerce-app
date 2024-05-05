52627a35e50c0b42e0c4a31e90f44c7ed4fdbbd0
KOSASIH <kosasihg88@gmail.com>
2024-05-05 11:25:32 +0700

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// models/user.js
+import { DataTypes } from 'sequelize';
+import db from '../config/database';
+
+const User = db.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
+
+export default User;
