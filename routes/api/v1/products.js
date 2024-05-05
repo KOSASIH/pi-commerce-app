76ef65356d7f5f6a99cf2f0ed0c57f4b8aef7aaf
KOSASIH <kosasihg88@gmail.com>
2024-05-05 11:27:01 +0700

    Create products.js

---
+++ routes/api/v1/products.js
@@ -0,0 +1,13 @@
+// routes/api/v1/products.js
+import express from 'express';
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/products';
+
+const router = express.Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
